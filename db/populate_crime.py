d0cf8a6b32208be8c4a571612423f7a42d195b95
Paul Dhanda <pdhanda@minesense.com>
2021-02-17 19:13:22 -0800

    fixed error handling

--- db/populate_crime.py
+++ db/populate_crime.py
@@ -1,5 +1,6 @@
 #  this file will be to populate distances using google maps api
 import pandas as pd
+import numpy as np
 import sqlite3
 from pathlib import Path
 import os
@@ -11,16 +12,19 @@ gmaps = googlemaps.Client(key=key)
 def get_lon_lat(row):
     address_string = row['HUNDRED_BLOCK'] + ', Surrey, BC'
     geocode_result = gmaps.geocode(address_string)
-    lon = geocode_result[0]['geometry']['location']['lng']
-    lat = geocode_result[0]['geometry']['location']['lat']
+    try:
+        lon = geocode_result[0]['geometry']['location']['lng']
+        lat = geocode_result[0]['geometry']['location']['lat']
+    except:
+        return pd.Series([np.nan,np.nan])
     return pd.Series([lon, lat])
 
 
 def main():
 
-    df_crime = pd.read_csv(os.path.join(os.path.dirname(__file__), 'raw_data', 'crime_2020.csv'), skiprows=range(1, 11), nrows=1000)
+    df_crime = pd.read_csv(os.path.join(os.path.dirname(__file__), 'raw_data', 'crime_2020.csv'), skiprows=range(1, 1011), nrows=1000)
     df_crime[['lon', 'lat']] = df_crime.apply(get_lon_lat, axis=1)
-    df_crime.to_csv(os.path.join(os.path.dirname(__file__), 'filter_data', 'crime.csv'), mode='a', index=False)
+    df_crime.to_csv(os.path.join(os.path.dirname(__file__), 'filter_data', 'crime.csv'), mode='a', index=False, header=False)
 
 
 if __name__ == "__main__":
